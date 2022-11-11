173d58de47a5b3a0e30de7e378eba43039871110
Eero Antikainen <eero.antikainen@helsinki.fi>
2022-11-11 15:45:32 +0200

    api-kutsut toimivat, korttien sisäinen rakenne alkaa hahmottumaan

--- MTG-Cube-App/src/entities/UI.py
+++ MTG-Cube-App/src/entities/UI.py
@@ -1,17 +1,56 @@
 import cube_and_cards
+import string
+import requests
+import json
 
 INITIAL_ACTIONS = {
-    "1": "1 luo uusi cube",
-    "2": "lataa vanha cube"
+    1: "luo uusi cube",
+    2: "lataa vanha cube",
+    9: "toista komennot",
+    0: "lopeta"
 }
 
 CUBE_ACTIONS = {
     "1": "1 lisää kortti",
     "2": "2 printtaa cube",
+    "9": "9 toita komennot",
     "0": "0 lopeta"
 
 }
 
-class CubeUI:
-    def __init__(self):
-        pass
+def initialUI():
+    for i in INITIAL_ACTIONS:
+        print(f"'{i}' : {INITIAL_ACTIONS[i]}")
+    while True:
+        action = int(input("Anna komento: "))
+        if action == 0:
+            print("Heippa!")
+            break
+        if action == 1:
+            nimi = input("Nimeä Cube: ")
+            uusi_cube = cube_and_cards.Cube(nimi)
+            cubeUI(uusi_cube)
+        if action == 9:
+            for i in INITIAL_ACTIONS:
+                print(f"'{i}' : {INITIAL_ACTIONS[i]}")
+
+
+def cubeUI(cube: cube_and_cards.Cube):
+    for i in CUBE_ACTIONS:
+        print(f"'{i}' : {CUBE_ACTIONS[i]}")
+    while True:
+        action = int(input("Anna komento: "))
+        if action == 0:
+            print("Palataan")
+            break
+        if action == 1:
+            name = input("Kortin nimi: ")
+            if cube_and_cards.card_test(name):
+                cube.add_card(name)
+            else:
+                print("Kortin nimellä haku ei onnistunut")
+        if action == 2:
+            for i in cube.collection:
+                print(i)
+
+initialUI()
--- MTG-Cube-App/src/entities/cube_and_cards.py
+++ MTG-Cube-App/src/entities/cube_and_cards.py
@@ -1,5 +1,5 @@
-
-from string import ascii_lowercase
+import requests
+import json
 
 class Cube:
     #Tämä luokka toimii yksittäisenä kokoelmana, johon voi tallettaa kortteja
@@ -8,19 +8,52 @@ class Cube:
         self.collection = []
 
     def add_card(self, name):
-        if Card(name) not in self.collection:
-            self.collection.append(Card(name))
-        else:
+        initial_load = CardData(name).card_dict       
+        if initial_load['name'] in self.collection:
             print(f"{Card(name)} on jo cubessa")
+        else:
+            self.collection.append(Card(initial_load['name']))
 
     def __str__(self):
-        return self.collection #väliaikainen ratkaisu
-
+        return self.name
 
 class Card:
     #Tämä vastaa yksittäistä korttia. Kortille määritellään alustavasti nimi, mutta myöhemmin lisää ominaisuuksia
     def __init__(self, name:str):
-        self.name = name.ascii_lowercase
+        self.name = name
+        card_dict = CardData(name).card_dict
+        self.cmc = int(card_dict["cmc"])
+        self.type = card_dict["type_line"]
 
     def __str__(self):
         return self.name
+
+class CardData:
+    def __init__(self, name):
+        name_for_api = name.replace(" ","+")
+        card_data = requests.get(f"https://api.scryfall.com/cards/named?exact={name_for_api}")
+        if card_data.status_code == 200:
+            self.card_dict = json.loads(jprint(card_data.json()))
+        else:
+            return False    
+
+def card_test(name: str):
+    name_for_api = name.replace(" ","+")
+    response = requests.get(f"https://api.scryfall.com/cards/named?exact={name_for_api}")
+    if response.status_code == 200:
+        return True
+
+# response = requests.get("https://api.scryfall.com/cards/named?exact=black+lotus")
+# print(response.status_code)
+
+
+def jprint(obj):
+    #Converts json object to string
+    data = json.dumps(obj, sort_keys=True, indent=4)
+    return data
+    
+# print(jprint(response.json()))
+cube = Cube("any")
+cube.add_card("Black Lotus")
+for i in cube.collection:
+    print(i, i.cmc, i.type)
