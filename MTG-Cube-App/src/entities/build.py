3fe01a11d44adcef39192b0647774aef1e9f58f1
Eero Antikainen <eero.antikainen@helsinki.fi>
2022-11-29 18:04:58 +0200

    buildia korjattu siten, että sen voi ajaa ja se korjaa sovelluksen, jos on yritetty ajaa sovellus ilman buildia

--- MTG-Cube-App/src/entities/build.py
+++ MTG-Cube-App/src/entities/build.py
@@ -4,31 +4,28 @@ import sqlite3
 
 def build():
     if os.path.exists("src/entities/fetched_cards/fetched_cards.db"):
-        pass
-    else:
-        d_b = sqlite3.connect("src/entities/fetched_cards/fetched_cards.db")
-        d_b.isolation_level = None
-        d_b.execute(
-            "CREATE TABLE Cards (id INTEGER PRIMARY KEY, name TEXT, "+
-            "colors TEXT, color_identity TEXT, cmc INTEGER, mana_cost "+
-            "TEXT, type TEXT, keywords TEXT, oracle TEXT, image_uri TEXT, p_t TEXT );")
+        os.remove("src/entities/fetched_cards.db")
+    d_b = sqlite3.connect("src/entities/fetched_cards/fetched_cards.db")
+    d_b.isolation_level = None
+    d_b.execute(
+        "CREATE TABLE Cards (id INTEGER PRIMARY KEY, name TEXT, "+
+        "colors TEXT, color_identity TEXT, cmc INTEGER, mana_cost "+
+        "TEXT, type TEXT, keywords TEXT, oracle TEXT, image_uri TEXT, p_t TEXT );")
     if os.path.exists("src/entities/fetched_cards/temp.db"):
-        pass
-    else:
-        d_b = sqlite3.connect("src/entities/fetched_cards/temp.db")
-        d_b.isolation_level = None
-        d_b.execute(
-            "CREATE TABLE Cards (id INTEGER PRIMARY KEY, name TEXT, "+
-            "colors TEXT, color_identity TEXT, cmc INTEGER, mana_cost "+
-            "TEXT, type TEXT, keywords TEXT, oracle TEXT, image_uri TEXT, p_t TEXT );")
+        os.remove("src/entities/fetched_cards/temp.db")
+    d_b = sqlite3.connect("src/entities/fetched_cards/temp.db")
+    d_b.isolation_level = None
+    d_b.execute(
+        "CREATE TABLE Cards (id INTEGER PRIMARY KEY, name TEXT, "+
+        "colors TEXT, color_identity TEXT, cmc INTEGER, mana_cost "+
+        "TEXT, type TEXT, keywords TEXT, oracle TEXT, image_uri TEXT, p_t TEXT );")
     if os.path.exists("src/entities/fetched_cards/TestCube.db"):
-        pass
-    else:
-        d_b = sqlite3.connect("src/entities/fetched_cards/TestCube.db")
-        d_b.isolation_level = None
-        d_b.execute(
-            "CREATE TABLE Cards (id INTEGER PRIMARY KEY, name TEXT, "+
-            "colors TEXT, color_identity TEXT, cmc INTEGER, mana_cost "+
-            "TEXT, type TEXT, keywords TEXT, oracle TEXT, image_uri TEXT, p_t TEXT );")
+        os.remove("src/entities/fetched_cards/TestCube.db")
+    d_b = sqlite3.connect("src/entities/fetched_cards/TestCube.db")
+    d_b.isolation_level = None
+    d_b.execute(
+        "CREATE TABLE Cards (id INTEGER PRIMARY KEY, name TEXT, "+
+        "colors TEXT, color_identity TEXT, cmc INTEGER, mana_cost "+
+        "TEXT, type TEXT, keywords TEXT, oracle TEXT, image_uri TEXT, p_t TEXT );")
 
 build()
