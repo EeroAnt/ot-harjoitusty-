d3a48dafeac92914bc55d85a997fc4f5319d785e
Eero Antikainen <eero.antikainen@helsinki.fi>
2022-12-07 22:18:01 +0200

    ui jäsennelty uusiksi

---
+++ MTG-Cube-App/src/user_interface/cube_ui.py
@@ -0,0 +1,60 @@
+from entities.cube import Cube
+from entities.card import Card
+from printer.printer import print_list_imgs, print_list_table
+import os
+from data.saver_loader import save
+from user_interface.filter_ui import filter_ui
+
+CUBE_ACTIONS = {
+    1: "1 lisää kortti",
+    2: "2 lisää kortit tiedostosta",
+    3: "3 poista kortti",
+    4: "3 printtaa cube tekstinä",
+    5: "4 printtaa cube kuvina",
+    6: "6 suodata cube",
+    7: "7 tallenna cube",
+    0: "0 lopeta"
+}
+
+
+def cube_(cube: Cube):
+    while True:
+        for i in CUBE_ACTIONS:
+            print(f"'{i}' : {CUBE_ACTIONS[i]}")
+        action = int(input("Anna komento: "))
+        if action == 0:
+            print("Palataan")
+            break
+        if action == 1:
+            name = input("Kortin nimi: ")
+            if Card.card_test(name) == True:
+                cube.add_card(name)
+            else:
+                print("Kortin nimellä haku ei onnistunut")
+        if action == 2:
+            name = ("Tiedoston nimi: ")
+            cube.add_cards_from_list(name)
+        if action == 3:
+            name = input("Kortin nimi: ")
+            cube.remove_card(name)
+        if action == 4:
+            print_list_table(cube)
+        if action == 5:
+            print_list_imgs(cube)
+        if action == 6:
+            cube = filter_ui(cube)
+        if action == 7:
+            if os.path.exists(f"src/data/Saved_Cubes/{cube.name}.db"):
+                confirmation = input(
+                    "Tällä nimellä on jo cube olemassa. Haluatko"+
+                        " varmasti tallentaa sen päälle? Y/n: ")
+                if confirmation == "Y":
+                    print("Tallennettu")
+                    os.remove(f"src/data/Saved_Cubes/{cube.name}.db")
+                    save(cube)
+                    print("Tallennettu")
+                else:
+                    print("Ei tallennettu")
+            else:
+                save(cube)
+                print("Tallennettu")
---
+++ MTG-Cube-App/src/user_interface/initial_ui.py
@@ -0,0 +1,63 @@
+from entities.cube import Cube
+from user_interface.cube_ui import cube_ui
+import data.saver_loader as saver
+import os
+INITIAL_ACTIONS = {
+    1: "luo uusi cube",
+    2: "lataa vanha cube",
+    3: "luo cube tiedostosta",
+    0: "lopeta"
+}
+
+CUBE_ACTIONS = {
+    1: "1 lisää kortti",
+    2: "2 poista kortti",
+    3: "3 printtaa cube tekstinä",
+    4: "4 printtaa cube kuvina",
+    5: "5 tallenna cube",
+    6: "6 suodata cube",
+    0: "0 lopeta"
+}
+
+
+def initialUI():
+    while True:
+        for i in INITIAL_ACTIONS:
+            print(f"'{i}' : {INITIAL_ACTIONS[i]}")
+        action = int(input("Anna komento: "))
+        if action == 0:
+            print("Heippa!")
+            break
+        if action == 1:
+            name = input("Nimeä Cube: ")
+            new_cube = Cube(name)
+            cube_ui(new_cube)
+        if action == 2:
+            name = input("Ladattava Cube: ")
+            cube_ui(saver.load(name))
+        if action == 3:
+            txt_file_name = input("Tiedostonimi, josta lista haetaan: ")
+            if os.path.exists("src/card_list_text_files/"+txt_file_name):
+                name_for_cube = input("Nimi luotavalle Cubelle: ")
+                cube_ui(saver.load_from_list(name_for_cube, txt_file_name))
+            else:
+                print("Tällä nimellä ei löytynyt listaa. Sisällytithän '.txt' syötteeseen?"+"\n")
+
+# os.remove("src/entities/Saved_Cubes/Pallo.db")
+
+# kuutio = cube_and_cards.Cube("Pallo")
+# kuutio.add_card("Black Lotus")
+# kuutio.add_card("vampiric tutor")
+# kuutio.add_card("island")
+# kuutio.add_card("plains")
+# kuutio.add_card("forest")
+# print(kuutio.collection)
+# saver.save(kuutio)
+# for i in kuutio.collection:
+#     print(i)
+# lataus = load("Pallo")
+# print(lataus)
+# print(type(lataus))
+# print(lataus.collection)
+# for i in lataus.collection:
+#     print(i)
