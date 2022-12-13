d8ed567b81d356fc9dd70896d996d53cd0dd69b3
EeroAnt <117111419+EeroAnt@users.noreply.github.com>
2022-12-13 12:09:04 +0200

    Kommentaaria kevennetty

--- MTG-Cube-App/src/card_list_text_files/card_list_text_file_handler.py
+++ MTG-Cube-App/src/card_list_text_files/card_list_text_file_handler.py
@@ -2,13 +2,10 @@ import os
 
 # Tämä muuntaa tekstitiedoston sisällön listaksi. Rivinvaihto erottelee alkiot.
 def lister(filename):
-    # Tässä luodaan polku hakemistoon
     __location__ = os.path.realpath(
         os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    # Tässä avataan hakemistosta löytyvä tekstitiedosto
     with open(os.path.join(__location__, filename), "r", encoding='UTF-8') as file:
         list_of_cards = file.readlines()
-    # Alustetaan palautettava lista ja lisätään siihen rivit ilman rivinvaihtoja
     list_to_return = []
     for i in list_of_cards:
         list_to_return.append(i[:len(i)-1])
